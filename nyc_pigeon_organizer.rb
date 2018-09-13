a8606bc938ef503231bb36564484c2cbfd2798b1
Ryanbarker0 <ryangeorgebarker0@gmail.com>
2018-09-13 15:18:07 +0100

    method add

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = Hash.new
+  data.each do |detail, char|
+    char.each do |att, name|
+      name.each do |names|
+        if new_hash[names] == nil
+          new_hash[names] = {detail => nil}
+        end
+        if new_hash[names][detail] == nil
+          new_hash[names][detail] = [att.t_s]
+        else
+          new_hash[names][detail] << att.t_s
+        end
+      end
+    end
+  end
+  new_hash
+
 end
