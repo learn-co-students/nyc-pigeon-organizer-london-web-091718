15baead17ccd741376d5555a25393083026d2afb
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-09-16 01:19:24 +0100

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |characteristics, details|
+    details.each do |info, name|
+      name.each do |bird|
+        if pigeon_list.has_key?(bird) == false
+          pigeon_list[bird] = {}
+        end
+        if pigeon_list[bird].has_key?(characteristics) == false
+          pigeon_list[bird][characteristics] = []
+        end
+        pigeon_list[bird][characteristics] << info.to_s
+      end
+    end
+  end
+  pigeon_list
 end
